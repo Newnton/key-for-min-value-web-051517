3e8cba894fb5837622a01a4d7a22df36da9e8199
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-01 23:14:03 -0400

    finished lab

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash != {}
+    x = 10000000000000
+    y = ""
+    name_hash.each do |name, value|
+      if value < x
+        x = value
+      end
+    end
+    name_hash.each do |name, value|
+      if value == x
+        y = name
+      end
+    end
+    return y
+  end
 end
